e65e1c31be53e2d52015e378f4d2062d2ef3fcd4
pem2002 <your-email@associated-with-pem2002>
2025-06-26 10:31:41 +0600

    opening

--- config/db.js
+++ config/db.js
@@ -7,12 +7,14 @@ const pgp = pgpInit();
 
 
 const db = pgp({
-    host: 'localhost',
-    port: process.env.DB_PORT,
+    host:  process.env.DB_HOST,
+    port: 5432,     // default port
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    ssl: false,
+    ssl: {
+        rejectUnauthorized: false, //set to true if you want to reject unauthorized SSL certificates
+    }
 })
 
 
